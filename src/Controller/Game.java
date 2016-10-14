6481388faede18bf50700f708dc6a79f6d275b37
Grant Weaver <gtweaver4@gmail.com>
2016-10-13 22:37:33 -0400

    Cleaned up Game.java
    Made the code a bit cleaner by adding some methods and created a memory handler

--- src/Controller/Game.java
+++ src/Controller/Game.java
@@ -3,28 +3,59 @@ package Controller;
 import org.lwjgl.*;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+
+import application.Player;
+
 import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Mouse;
 
 import static org.lwjgl.opengl.GL11.*;
 
 public class Game {
-	
-	public static void runGame() {
-		
-		//this creates the window that will be running the game
+
+	private Player player = new Player(25, 25);
+
+	public void runGame() {
+
+		// this creates the window that will be running the game
+		initDisplay();
+
+		// this runs the games actions
+		gameLoop();
+
+		// this handles memory issue of if the game is closed
+		Display.destroy();
+
+	}
+
+	private static void initDisplay() {
 		try {
-			Display.create();
-      //pixel display at 1280x7200
 			Display.setDisplayMode(new DisplayMode(1280, 720));
-			Display.setTitle("test");
+			Display.create();
+			Display.setTitle("Robots are Invading!");
 		} catch (LWJGLException e) {
 			System.out.println("Display wasnt initialized correctly");
 		}
-		
-		//this runs the games actions
+	}
+
+	private void gameLoop() {
 		while (!Display.isCloseRequested()) {
-			Display.update();
-      //frames per second and update ratio
+			// game code goes here
+
+			glClear(GL_COLOR_BUFFER_BIT); // clears screen before drawing
+
+			player.drawPlayer();
+			player.playerMovement();
+
+			if (Mouse.isButtonDown(0)) {
+				player.shoot(Mouse.getX(), Mouse.getY());
+			}
+
+			if (Display.isCloseRequested()) {
+				System.exit(0);
+			}
+
+			Display.update(); // draw everything before the screen is updated
 			Display.sync(60);
 		}
 	}
