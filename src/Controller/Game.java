814260181e2ddf7b869381e1bbdd57455a722c74
Grant Weaver <gtweaver4@gmail.com>
2016-10-20 12:01:44 -0400

    updated game.java
    this creates the game loop for mirea's code instead of using opengl functions it uses slick

--- src/Controller/Game.java
+++ src/Controller/Game.java
@@ -4,60 +4,53 @@ import org.lwjgl.*;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 
-import application.Player;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.Image;
+
+import Model.Enemy;
+import Model.Player;
 
 import org.lwjgl.LWJGLException;
 import org.lwjgl.input.Mouse;
 
 import static org.lwjgl.opengl.GL11.*;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Game {
 
-	private Player player = new Player(25, 25);
+	public static final int width=800;
+	public static final int height=600;
 
-	public void runGame() {
+	public static AppGameContainer appgc;
 
-		// this creates the window that will be running the game
-		initDisplay();
+	//this is where the gameloop is located
+	public void run()
+	{
 
-		// this runs the games actions
-		gameLoop();
+		try
+		{
 
-		// this handles memory issue of if the game is closed
-		Display.destroy();
+			appgc = new AppGameContainer(new Enemy("Enemy"));
+			appgc.setDisplayMode(width, height, false);
+			appgc.start();
 
-	}
 
-	private static void initDisplay() {
-		try {
-			Display.setDisplayMode(new DisplayMode(1280, 720));
-			Display.create();
-			Display.setTitle("Robots are Invading!");
-		} catch (LWJGLException e) {
-			System.out.println("Display wasnt initialized correctly");
 		}
-	}
-
-	private void gameLoop() {
-		while (!Display.isCloseRequested()) {
-			// game code goes here
-
-			glClear(GL_COLOR_BUFFER_BIT); // clears screen before drawing
-
-			player.drawPlayer();
-			player.playerMovement();
-
-			if (Mouse.isButtonDown(0)) {
-				player.shoot(Mouse.getX(), Mouse.getY());
-			}
-
-			if (Display.isCloseRequested()) {
-				System.exit(0);
-			}
-
-			Display.update(); // draw everything before the screen is updated
-			Display.sync(60);
+		catch (SlickException ex)
+		{
+			Logger.getLogger(Enemy.class.getName()).log(Level.SEVERE, null, ex);
 		}
 	}
 
 }
+
+
